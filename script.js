eaffe86b3d5768e8b24cac27fc8cce381d816eb3
jared0192galicia <jared0192galicia@gmail.com>
2022-08-29 01:43:52 -0500

    Integración de clase 8 en proceso

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const desktopMenu = document.querySelector(".desktop-menu");
 const navEmail = document.querySelector(".navbar-email");
-// let active = false;
+let active = false;
 const buttonMobileMenu = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenu = document.getElementById("menuMobile");
 
 function run(){
     navEmail.onclick = switchDesktopMenu;
@@ -14,9 +14,21 @@ function run(){
 function switchDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
 }
+/**
+ * `if (active) { mobileMenu.classList.toggle('none'); } else { mobileMenu.style.display = 'block' }`
+ * 
+ * The `if` statement is checking to see if the `active` variable is true. If it is, then it's going to
+ * toggle the `none` class on the `mobileMenu` element. If it's not, then it's going to set the
+ * `display` property of the `mobileMenu` element to `block`
+ */
 function switchMobileMenu(){
-    // mobileMenu.classList.toggle("inactive");
-    console.log("🚀 ~ file: script.js ~ line 6 ~ mobileMenu", mobileMenu)
+    console.log("🚀 ~ file: script.js ~ line 6 ~ mobileMenu", mobileMenu);
+    // mobileMenu.classList.toggle('inactive');
+    if (active) {
+        mobileMenu.classList.toggle('none');
+    } else {
+        mobileMenu.style.display = 'block'
+    }
 }
 
 run();
