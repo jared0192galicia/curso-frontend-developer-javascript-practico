fcdcf7d44b497c1f0499f4d47c6217c220f7f97a
jared0192galicia <jared0192galicia@gmail.com>
2022-08-29 02:18:45 -0500

    Integrando clase 8 | mostrar y ocultar menu mobile

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const navEmail = document.querySelector(".navbar-email");
 let active = false;
 const buttonMobileMenu = document.querySelector(".menu");
-const mobileMenu = document.getElementById("menuMobile");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 function run(){
     navEmail.onclick = switchDesktopMenu;
-    buttonMobileMenu.onclick = switchMobileMenu;
+    buttonMobileMenu.addEventListener('click', switchMobileMenu);
 }
 /**
  * When the user clicks on the hamburger menu, toggle the inactive class on the desktop menu.
@@ -18,17 +18,21 @@ function switchDesktopMenu(){
  * `if (active) { mobileMenu.classList.toggle('none'); } else { mobileMenu.style.display = 'block' }`
  * 
  * The `if` statement is checking to see if the `active` variable is true. If it is, then it's going to
- * toggle the `none` class on the `mobileMenu` element. If it's not, then it's going to set the
+ * toggle the `none` class on the `mobileMenu` elem ent. If it's not, then it's going to set the
  * `display` property of the `mobileMenu` element to `block`
  */
 function switchMobileMenu(){
+    console.log("🚀 ~ file: script.js ~ line 5 ~ buttonMobileMenu", buttonMobileMenu);
     console.log("🚀 ~ file: script.js ~ line 6 ~ mobileMenu", mobileMenu);
-    // mobileMenu.classList.toggle('inactive');
-    if (active) {
-        mobileMenu.classList.toggle('none');
-    } else {
-        mobileMenu.style.display = 'block'
-    }
+    mobileMenu.classList.toggle("inactive");
+    // if (active) {
+    //     mobileMenu.classList.toggle('none');
+    // } else {
+    //     mobileMenu.style.display = 'block'
+    // }
+    
+    console.log("🚀 ~ file: script.js ~ line 5 ~ buttonMobileMenu", buttonMobileMenu);
+    console.log("🚀 ~ file: script.js ~ line 6 ~ mobileMenu", mobileMenu);
 }
 
 run();
