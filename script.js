be7ccf7a443a7b0bb625816a351375290f49aaca
jared0192galicia <jared0192galicia@gmail.com>
2022-09-29 15:52:01 -0500

    Retomado class fusion mobile

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const buttonMobileMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 function run(){
+    
+    console.log("🚀 ~ file: script.js ~ line 6 ~ mobileMenu", mobileMenu);
+    buttonMobileMenu.onclick = switchMobileMenu;
     navEmail.onclick = switchDesktopMenu;
-    buttonMobileMenu.addEventListener('click', switchMobileMenu);
 }
 /**
  * When the user clicks on the hamburger menu, toggle the inactive class on the desktop menu.
@@ -14,25 +16,9 @@ function run(){
 function switchDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
 }
-/**
- * `if (active) { mobileMenu.classList.toggle('none'); } else { mobileMenu.style.display = 'block' }`
- * 
- * The `if` statement is checking to see if the `active` variable is true. If it is, then it's going to
- * toggle the `none` class on the `mobileMenu` elem ent. If it's not, then it's going to set the
- * `display` property of the `mobileMenu` element to `block`
- */
+
 function switchMobileMenu(){
-    console.log("🚀 ~ file: script.js ~ line 5 ~ buttonMobileMenu", buttonMobileMenu);
-    console.log("🚀 ~ file: script.js ~ line 6 ~ mobileMenu", mobileMenu);
     mobileMenu.classList.toggle("inactive");
-    // if (active) {
-    //     mobileMenu.classList.toggle('none');
-    // } else {
-    //     mobileMenu.style.display = 'block'
-    // }
-    
-    console.log("🚀 ~ file: script.js ~ line 5 ~ buttonMobileMenu", buttonMobileMenu);
-    console.log("🚀 ~ file: script.js ~ line 6 ~ mobileMenu", mobileMenu);
 }
 
 run();
